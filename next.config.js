2012756aac3ecbc75f021d377230a7a315b3417d
leemda <1eemda@naver.com>
2023-10-05 20:13:30 +0900

    svgr 웹팩 추가 및 next.config.js 수정

--- next.config.js
+++ next.config.js
@@ -3,4 +3,14 @@ const nextConfig = {
   reactStrictMode: true,
 }
 
-module.exports = nextConfig
+module.exports = {
+  ...nextConfig, // 이전에 정의한 nextConfig 객체를 병합
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    });
+
+    return config;
+  },
+};
--- src/components/sideMenu/mainSideMenu.js
+++ src/components/sideMenu/mainSideMenu.js
@@ -1,9 +1,85 @@
 import styled from "styled-components";
+import Alerts from '../../../public/asset/icons/Alerts.svg';
+import Badge from '../../../public/asset/icons/Badge.svg';
+import Bank from '../../../public/asset/icons/Bank.svg';
+import Calendar from '../../../public/asset/icons/Calendar.svg';
+import Chat from '../../../public/asset/icons/Chat.svg';
+import Forms from '../../../public/asset/icons/Forms.svg';
+import Megaphone from '../../../public/asset/icons/Megaphone.svg';
+import Time from '../../../public/asset/icons/Time.svg';
+import Video_Conference from '../../../public/asset/icons/Video_Conference.svg';
+import Working_Together from '../../../public/asset/icons/Working_Together.svg';
+
+function MenuToggle({ menus }) {
+    return (
+      <div>
+        {menus.icon}
+        <span>{menus.value}</span>
+      </div>
+    );
+  }
 
 export default function MainSideMenu() {
+    const menus = [
+        {   
+            id: 1,
+            icon: <Alerts />,
+            value: '알림'
+        },
+        {
+            id: 2,
+            icon: <Video_Conference />,
+            value: '워크스페이스'
+        },
+        {
+            id: 3,
+            icon: <Time />,
+            value: '근태관리'
+        },
+        {   
+            id: 4,
+            icon: <Calendar />,
+            value: '캘린더'
+        },
+        {
+            id: 5,
+            icon: <Chat />,
+            value: '메신저'
+        },
+        {
+            id: 6,
+            icon: <Megaphone />,
+            value: '공지사항'
+        },
+        {
+            id: 7,
+            icon: <Forms />,
+            value: '전자결재'
+        },
+        {
+            id: 8,
+            icon: <Badge />,
+            value: '인사관리'
+        },
+        {
+            id: 9,
+            icon: <Bank />,
+            value: '급여관리'
+        },
+        {
+            id: 10,
+            icon: <Working_Together />,
+            value: '커뮤니티'
+        },
+    ];
+
     return (
         <SideMenu>
-            test
+            <MenuIcons>
+                {menus.map((menu) => (
+                    <MenuToggle menus={menu} key={menu.id} />
+                ))}
+            </MenuIcons>
         </SideMenu>
     )
 }
@@ -12,4 +88,8 @@ const SideMenu = styled.div`
     width: 80px;
     height: 100vh;
     background-color: #005FC5;
+`;
+
+const MenuIcons = styled.div`
+
 `;
