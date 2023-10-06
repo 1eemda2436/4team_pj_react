4611181cc878a4eb949f9b0b45f27ab539f04ad8
leemda <1eemda@naver.com>
2023-10-06 15:14:23 +0900

    next.config.js 스타일 컴포넌트 오류 수정 및 로그인 페이지 추가

--- next.config.js
+++ next.config.js
@@ -1,11 +1,13 @@
+
 const nextConfig = {
   reactStrictMode: true,
+  styledComponents: true,
   webpack: (config) => {
     config.module.rules.push({
-      test: /\.svg$/,
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
       use: ['@svgr/webpack'],
     });
-
     return config;
   },
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,20 @@
-import styled from "styled-components"
+import styled from "styled-components";
+import Logo2 from '../../public/asset/icons/logo2.svg';
+import { useRouter } from "next/router";
 
 export default function Home() {
+  const router = useRouter();
+
   return (
     <Component>
-      test
+      <Logo2Icon />
+      <LoginInputBox>
+        <LoginInput placeholder="ID" type="text" />
+        <LoginInput placeholder="PASSWORD" type="password" />
+
+        <LoginBtn> login </LoginBtn>
+        <JoinBtn onClick={() => router.push('/join')}>사업자 등록</JoinBtn>
+      </LoginInputBox>
     </Component>
   )
 }
@@ -11,4 +22,62 @@ export default function Home() {
 const Component = styled.div`
   width: 100%;
   height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const Logo2Icon = styled(Logo2)`
+  margin-top: 117px;
+`;
+
+const LoginInputBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 600px;
+  margin-top: 48px;
+`;
+
+const LoginInput = styled.input`
+  border-radius: 5px;
+  border: 1px solid #CBCBCB;
+  background: #F8F8F8;
+  width: 100%;
+  height: 72px;
+  display: flex;
+  align-items: center;
+  justify-content: start;
+  margin-bottom: 15px;
+  font-size: 18px;
+  padding-left: 22px;
+
+  ::placeholder {
+    margin-left: 22px;
+    color: #D9D9D9;
+  }
+`;
+
+const LoginBtn = styled.div`
+  border-radius: 5px;
+  border: 1px solid #CBCBCB;
+  background: #007BFF;
+  width: 100%;
+  height: 72px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: white;
+  font-size: 28px;
+  font-weight: 600;
+  margin-top: 28px;
+  cursor: pointer;
+  padding: 0px 11px;
+`;
+
+const JoinBtn = styled.div`
+  color: #5C9DFF;
+  font-size: 18px;
+  font-weight: 700;
+  margin-top: 21px;
+  cursor: pointer;
 `;
