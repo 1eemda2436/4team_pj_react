da023e736f3fa335a81e9dcb9d7c1864ce9d820e
garamJung <jgr140@gmail.com>
2023-10-08 18:32:06 +0900

    집에서 마저 할게요

---
+++ src/pages/doc/circularList.js
@@ -0,0 +1,100 @@
+import MainLayout from "@/components/layout/mainLayout"
+import styled from "styled-components";
+
+
+
+const Doc = () => {
+    return(
+        <Container>
+            <Title>
+                <H1>회람 문서함</H1>
+            </Title>
+            <Docstyle2>
+                <Table>
+                    <thead>
+                        <TableTr>
+                            <TableTh2>문서번호</TableTh2>
+                            <TableTh2 isTitle>문서 제목</TableTh2>
+                            <TableTh2>조회여부</TableTh2>
+                        </TableTr>
+                    </thead>
+                    <tbody>
+                        <TableTr>
+                                <TableTd2>1</TableTd2>
+                                <TableTd2 isTitle>1</TableTd2>
+                                <TableTd2>1</TableTd2>
+                        </TableTr>
+                    </tbody>
+                </Table>
+            </Docstyle2>
+        </Container>
+    )
+}
+
+export default Doc;
+
+Doc.getLayout = function getLayout(page) {
+    return <MainLayout>{page}</MainLayout>;
+};
+
+const Container = styled.div`
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+`;
+
+const Title = styled.div`
+    text-align: center;
+    margin-bottom: 20px;
+`;
+
+const Docstyle1 = styled.div`
+    display: flex;
+    justify-content: flex-start;
+    margin: 10px;
+`;
+
+const Docstyle2 = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    margin: 10px;
+`;
+
+const H1 = styled.h1`
+    font-size: 30px;
+`;
+
+const Table = styled.table`
+    border: 1px solid;
+`;
+
+const TableTr = styled.tr`
+    border: 1px solid;
+`;
+
+const TableTh = styled.th`
+    border: 1px solid;
+    padding: 5px;
+`;
+
+const TableTh2 = styled.th`
+    border: 1px solid;
+    padding-left: 10px;
+    padding-right: 10px;
+    width: ${(props) => (props.isTitle ? '60%' : 'auto%')};
+    width: 100px;
+`;
+
+const TableTd = styled.td`
+    border: 1px solid;
+`;
+
+const TableTd2 = styled.td`
+    border: 1px solid;
+    padding-left: 10px;
+    padding-right: 10px;
+    width: ${(props) => (props.isTitle ? '60%' : 'auto%')};
+`;
