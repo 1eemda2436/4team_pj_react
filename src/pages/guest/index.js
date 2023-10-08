664cac5c32ddbd7c61aca81952462f263cc7888c
leemda <1eemda@naver.com>
2023-10-08 11:34:11 +0900

    layout 관련 주석 추가

--- src/pages/guest/index.js
+++ src/pages/guest/index.js
@@ -10,6 +10,10 @@ const Guest = () => {
 
 export default Guest;
 
+/**
+ * 컴포넌트명.getLayout 으로 _app.js에 있는 함수 선언
+ * 리턴 값에 사용할 레이아웃 태그 선언 후 매개변수로 받은 page값 선언해서 사용
+ */
 Guest.getLayout = function getLayout(page) {
     return <MainLayout>{page}</MainLayout>;
 };
