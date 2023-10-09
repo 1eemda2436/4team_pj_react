a9b3cac69071e175539c4a8860848401857c548d
yujin <dladbwls1302@naver.com>
2023-10-09 17:49:52 +0900

    아이콘추가

---
+++ src/pages/chatting/ChattingMain.js
@@ -0,0 +1,21 @@
+
+import styled from "styled-components";
+
+const ChattingMain = () => {
+    return (
+        <Component>
+            <Chatdiv>
+                뽀엥
+            </Chatdiv>
+        </Component>
+    );
+}
+
+const Component = styled.div``;
+
+const Chatdiv = styled.div `
+    border: 1px solid black;
+
+`;
+
+export default ChattingMain;
